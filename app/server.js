2f4fa19bccfe3455595b010d7827ee6dc313b3e9
dehnel <adam.dehnel@biworldwide.com>
2016-11-04 22:26:09 -0500

    Added real url back in

--- app/server.js
+++ app/server.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 
 const config = {
 	"TSG-FED/demo-docs": {
-		"url": "<slack endpoint here>",
+		"url": "/services/T18CYMQSU/B2YB01JSF/jQJx96Q0UO1ieGbwVpH2OvR3",
 		"channel": "github-hook-example"
 	}
 }
